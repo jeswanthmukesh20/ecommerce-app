d2f2600262642e429ca70587e2de0cd638184103
ashwin3005 <sashwin@protonmail.com>
2023-12-13 00:10:12 +0530

    check out

---
+++ frontend/src/pages/Checkout.vue
@@ -0,0 +1,19 @@
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-success text-white">
+                    <h2>Checkout Complete</h2>
+                </div>
+                <div class="card-body">
+                    <p class="lead">Thank you for shopping with us!</p>
+                    <p>Your order has been successfully processed.</p>
+                    <p>An email confirmation has been sent to your email address.</p>
+                    <!-- You can add more information about the order, delivery, etc. here -->
+
+                    <a href="#" class="btn btn-primary">Continue Shopping</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
